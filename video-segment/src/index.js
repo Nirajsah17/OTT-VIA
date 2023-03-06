c2cd3442621973dc6ff906a896b3cd2a80a76c6d
Robin <robinrj39@gmail.com>
2023-03-07 00:28:29 +0530

    convert uploaded video in different resolution

--- video-segment/src/index.js
+++ video-segment/src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ffmpeg = require('fluent-ffmpeg');
 const multer = require('multer');
-const fs=require('fs');
-const upload = multer({ dest: 'tmp/' });
+const fs = require('fs');
+const upload = multer({ dest: 'D:/streamClone/path/tmp/' });
 const app = express();
 app.use(express.json());
 const { spawn } = require('child_process');
@@ -13,26 +13,30 @@ const { dirname } = require('path');
 
 
 
-  
-    
+
+
+app.get("/whoami", (req, res) => {
+    res.send("video segmenter apis");
+})
 
 
 
 
+app.get("/upload", upload.single('video'), (req, res, next) => {
+    // const outputFilePath = 'D:/streamClone/path/to/output_video.mp4';
+    const outputFilePath = 'D:/streamClone/path/to/';
+    const filePath = req.file.path;
+    const executefile = 'D:\\streamClone\\OTT-VIA\\video-segment\\src\\shellscript.bat ' + req.file.path + ' ' + outputFilePath;
+    exec(executefile, { shell: true }, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            res.send(err);
+        }
+        console.log(stdout);
+    });
 
-app.get("/upload" , upload.single('video'), (req, res, next)=>{
-const outputFilePath = 'D:/streamClone/path/to/output_video.mp4';
-const filePath=req.file.path;
-const executefile='D:\\streamClone\\OTT-VIA\\video-segment\\src\\shellscript.bat ' +req.file.path +' '+ outputFilePath;
-exec(executefile,{ shell: true }, (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
-         res.send(err);
-             }
-  console.log(stdout);
-});
-res.send("User upload video successfully")
- })
+    res.send("User upload video successfully")
+})
 
 
 app.listen(3000, () => {
